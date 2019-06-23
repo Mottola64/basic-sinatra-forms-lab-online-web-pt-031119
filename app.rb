ed3ffdd1cbed1f9fd1364ebce85dac38566bd170
Michael Mottola <mottola64@gmail.com>
2019-06-23 21:35:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,14 @@ get '/newteam' do
   erb :newteam
 end
 
-get '/team' do
+post '/team' do
+  @name = params[:name]
+  @coach = params[:coach]
+  @pg = params[:pg]
+  @sg = params[:sg]
+  @sf = params[:sf]
+  @pf = params[:pf]
+  @c = params[:c]
   erb :team
 end
 end
